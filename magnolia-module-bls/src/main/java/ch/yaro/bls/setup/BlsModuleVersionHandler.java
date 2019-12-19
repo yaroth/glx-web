7d8df696f528d15041235be7c3703223627fd0f4
Yann Roth <yann.roth@gmx.net>
2019-12-19 22:39:35 +0100

    disable publishing to magnoliaPublic8080 on first install.

--- magnolia-module-bls/src/main/java/ch/yaro/bls/setup/BlsModuleVersionHandler.java
+++ magnolia-module-bls/src/main/java/ch/yaro/bls/setup/BlsModuleVersionHandler.java
@@ -7,6 +7,7 @@ import info.magnolia.module.InstallContext;
 import info.magnolia.module.delta.*;
 import info.magnolia.nodebuilder.task.ErrorHandling;
 import info.magnolia.repository.RepositoryConstants;
+import info.magnolia.setup.initial.GenericTasks;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,16 +30,25 @@ public class BlsModuleVersionHandler extends DefaultModuleVersionHandler {
 
 
 
+//    @Override
+//    protected List<Task> getExtraInstallTasks(InstallContext installContext) {
+//
+//        ArrayList<Task> tasks = new ArrayList<>();
+//        tasks.addAll(super.getExtraInstallTasks(installContext));
+//
+//        tasks.add(new SetPropertyTask("Disable publishing to magnoliaPublic8080", RepositoryConstants.CONFIG, "/modules/publishing-core/config/receivers/magnoliaPublic8080", "enabled", "false"));
+//
+//        return tasks;
+//    }
+
     @Override
-    protected List<Task> getExtraInstallTasks(InstallContext installContext) {
-
-        ArrayList<Task> tasks = new ArrayList<>();
-        tasks.addAll(super.getExtraInstallTasks(installContext));
-
+    protected List<Task> getBasicInstallTasks(InstallContext ctx) {
+        final List<Task> tasks = new ArrayList<>(super.getBasicInstallTasks(ctx));
         tasks.add(new SetPropertyTask("Disable publishing to magnoliaPublic8080", RepositoryConstants.CONFIG, "/modules/publishing-core/config/receivers/magnoliaPublic8080", "enabled", "false"));
 
         return tasks;
     }
+
 
 
 
